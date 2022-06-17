7a92959ea58e7da34c7dbc4602ac87bf4847a9db
Eduardo <epila8661@uta.edu.ec>
2022-06-17 15:19:36 -0500

    Cambios Pila

--- Grupo3_v2/src/grupo3_v2/Flores.java
+++ Grupo3_v2/src/grupo3_v2/Flores.java
@@ -10,5 +10,9 @@ package grupo3_v2;
  * @author DELLWIN10
  */
 public class Flores {
-    
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hola mundo "+i);
+        }
+    }
 }
