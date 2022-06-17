25408459bf6d29ade86e973b02fc27a5059aefca
David Flores <eflores9091@uta.edu.ec>
2022-06-17 15:07:14 -0500

    Se añadio la clase Flores

---
+++ Grupo3_v2/src/grupo3_v2/Flores.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grupo3_v2;
+
+/**
+ *
+ * @author DELLWIN10
+ */
+public class Flores {
+    
+}
